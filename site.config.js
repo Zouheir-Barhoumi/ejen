5b331d9e59b1993ef3d1f2736c9609e0248b12a0
Zouheir-Barhoumi <zouheir.barhoumi@gmail.com>
2024-09-20 19:32:07 +0200

    simple config data

---
+++ site.config.js
@@ -0,0 +1,9 @@
+const projects = require('./src/data/projects')
+
+module.exports = {
+    site: {
+        title: 'Ejen',
+        description: 'Micro Static Site Generator in Node.js',
+        projects,
+    },
+}
