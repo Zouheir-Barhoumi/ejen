7da7b5bc61935765868c874b8e5a1d73e909a2e6
Zouheir-Barhoumi <zouheir.barhoumi@gmail.com>
2024-09-20 19:03:52 +0200

    comentated the build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,52 +1,64 @@
+// Import the fs-extra module for file system operations
 const fse = require('fs-extra')
+
 const path = require('path')
+
+// Import the promisify function from the util module to convert callback-based functions to promise-based
 const { promisify } = require('util')
+
+// Convert the ejs.renderFile function to a promise-based function
 const ejsRenderFile = promisify(require('ejs').renderFile)
+
+// Convert the glob function to a promise-based function
 const globP = promisify(require('glob'))
+
+// Import the site configuration
 const config = require('../site.config')
 
 const srcPath = './src'
-const distPath = './public'
 
-// clear destination folder
-fse.emptyDirSync(distPath)
+const destPath = './public'
 
-// copy assets folder
-fse.copy(`${srcPath}/assets`, `${distPath}/assets`)
+fse.emptyDirSync(destPath)
 
-// read page templates
+// Copy the assets folder from source to destination
+fse.copy(`${srcPath}/assets`, `${destPath}/assets`)
+
+// Find all .ejs files in the src/pages directory
 globP('**/*.ejs', { cwd: `${srcPath}/pages` })
-  .then((files) => {
-    files.forEach((file) => {
-      const fileData = path.parse(file)
-      const destPath = path.join(distPath, fileData.dir)
+    .then((files) => {
+        // Iterate through each found file
+        files.forEach((file) => {
+            // Parse the file path to get file information
+            const fileData = path.parse(file)
+            // Construct the destination path for the file
+            const destPath = path.join(destPath, fileData.dir)
 
-      // create destination directory
-      fse
-        .mkdirs(destPath)
-        .then(() => {
-          // render page
-          return ejsRenderFile(
-            `${srcPath}/pages/${file}`,
-            Object.assign({}, config)
-          )
-        })
-        .then((pageContents) => {
-          // render layout with page contents
-          return ejsRenderFile(
-            `${srcPath}/layout.ejs`,
-            Object.assign({}, config, { body: pageContents })
-          )
-        })
-        .then((layoutContent) => {
-          // save the html file
-          fse.writeFile(`${destPath}/${fileData.name}.html`, layoutContent)
-        })
-        .catch((err) => {
-          console.error(err)
+            // Create the destination directory
+            fse.mkdirs(destPath)
+                .then(() => {
+                    // Render the page template with the site configuration
+                    return ejsRenderFile(
+                        `${srcPath}/pages/${file}`, Object.assign({}, config)
+                    )
+                })
+                .then((pageContents) => {
+                    // Render the layout template with the site configuration and page contents
+                    return ejsRenderFile(
+                        `${srcPath}/layout.ejs`, Object.assign({}, config, { body: pageContents })
+                    )
+                })
+                .then((layoutContent) => {
+                    // Write the rendered HTML to the destination file
+                    fse.writeFile(`${destPath}/${fileData.name}.html`, layoutContent)
+                })
+                .catch((err) => {
+                    // Log any errors that occur during the process
+                    console.error(err)
+                })
         })
     })
-  })
-  .catch((err) => {
-    console.error(err)
-  })
+    .catch((err) => {
+        // Log any errors that occur during the file search
+        console.error(err)
+    })
